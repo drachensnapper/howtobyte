21d58ea5cb1bfa06ac402ec9a7183377987754e5
andreaskurz <andreas.kurz@haw-ingolstadt.de>
2013-12-23 00:22:27 +0100

    Update gehirn.js

--- gehirn.js
+++ gehirn.js
@@ -1,12 +1,50 @@
 // This moduile offers functions to convert one number to the same number in a nother representation
 
-
+// binary >>
 function bin2dec( binary ) 
 {
   return decimal;
 }
 
-function bin2Hex( binary )
+function bin2hex( binary )
 {
   return hex;
 }
+
+function bin2ascii( binary )
+{
+  return ascii;
+}
+
+// decimal >>
+function dec2bin( decimal )
+{
+  return binary;
+}
+
+function dec2hex( decimal )
+{
+  return hex;
+}
+
+function dec2ascii( decimal )
+{
+  return ascii;
+}
+
+// hex >>
+function hex2bin( hex )
+{
+  return binary;
+}
+
+function hex2dec( hex )
+{
+  return dec;
+}
+
+function hex2ascii( hex )
+{
+  return ascii;
+}
+
